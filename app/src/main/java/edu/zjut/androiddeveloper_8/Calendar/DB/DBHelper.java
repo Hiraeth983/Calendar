bc0e57844eef42a38688e09f567974c81edc803f
张拓 <1102168414@qq.com>
2022-05-30 08:49:06 +0800

    数据库版本号改为1

--- app/src/main/java/edu/zjut/androiddeveloper_8/Calendar/DB/DBHelper.java
+++ app/src/main/java/edu/zjut/androiddeveloper_8/Calendar/DB/DBHelper.java
@@ -11,7 +11,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "calendar.db";
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 1;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
